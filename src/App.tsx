a9ef53810e82dff7d735d9dbb6696cb4c61c0c34
orihiro <kimhijin1@naver.com>
2024-01-24 23:07:01 +0900

    css rough

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import WritePage from "./pages/WritePage";
+import DetailPage from "./pages/DetailPage";
+import LoginPage from "./pages/LoginPage";
+import PorfilePage from "./pages/PorfilePage";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<PorfilePage />} />
+      </Routes>
+      <NavigationBar />
     </div>
   );
 }
